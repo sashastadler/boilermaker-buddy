f7d07bf2bd7fc81e2913aed2c4cb7958e00589eb
Roger Phaneuf <rogerdphan@gmail.com>
2022-03-23 13:08:58 -0400

    Made changes to SQL section to send all Student Calendar information to student_calendar table in database, and do error handling

--- scrape.py
+++ scrape.py
@@ -2,13 +2,26 @@ from bs4 import BeautifulSoup
 import mysql.connector
 import requests
 import re
+from selenium import webdriver
+from selenium.webdriver.support import ui
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+##
+# SELENIUM SETUP
+##
+
+driver = webdriver.Chrome(executable_path='C:\Webdrivers\chromedriver')
+
+##
+# STUDENT CALENDAR
+##
 
 r = requests.get('https://www.purdue.edu/registrar/calendars/2021-22-Academic-Calendar.html')
 data = r.text
 soup = BeautifulSoup(data, 'html.parser')
 # Main content: div class="maincontent col-lg-9 col-md-9 col-sm-9 right"
 # month  <h4>
-
 # <table class "calendarTable"
 # day: 'day noGutterLeft col-lg-1 col-md-2 col-sm-2 col-xs-3'
 # day of the week: 'weekDay noGutterLeft col-lg-1 col-md-2 col-sm-2 col-xs-3'
@@ -46,54 +59,117 @@ for month in soup.find_all(class_='calendarTable'):
                     content = '0' + content
                 if (monthInd < 5 and len(content) == 2):
                     dates.append('2021-' + months[monthInd] + '-' + content)
-                elif(len(content) == 2):
+                else: #if(len(content) == 2):
                     dates.append('2022-' + months[monthInd] + '-' + content)
 
             elif (i == 1):  #Description (time & event)
                 # Extract Time #
                 time = re.match('(\d*:*\d* [ap].m.)', content)
                 if time:
-                    times.append(time)
-                    # print(time.group(1))
+                    times.append(time.group(0))
                 else:
-                    times.append(None)  # TODO make sure that this doesn't just append nothing
+                    times.append(None)
                 # Extract Event Description #
                 desc = re.match('(\d*:*\d* [ap].m.)*([a-zA-Z0-9!@#\\$%\\^\\&*\\)\\(+=\/_\-, ]*)(\n)*', content)
                 if desc:
                     descriptions.append(desc.group(2).lstrip())
                 else:
                     descriptions.append(None)
+
             elif (i == 2):  # Weekday
                 weekday = re.match('([A-Za-z])*(-([A-Za-z]*))*', content)
                 if weekday:
-                    weekdays.append(weekday)
-                    print(weekday)
+                    weekdays.append(weekday.group(0))
                 else:
                     weekdays.append(None)
 
     monthInd += 1
 
-tempTuple = ()
-tempDates = []
+##
+# STUDENT DINING
+##
 
-for date in dates:
-    tempTuple += tuple([date])
+courts = {} # Dictionary of dictionaries - each sub dictionary details stations(key) and meals(values)
+courtURLs = []
+diningList = []
 
-data = [x for x in zip(*[iter(dates)])]
+# MENU #
+# TODO include for loop of dining courts - testing with Earhart
 
-mydb = mysql.connector.connect(host="boilermakerbuddydb.c8jck5ubwnj5.us-east-1.rds.amazonaws.com",\
+baseDiningURL = 'https://dining.purdue.edu/menus/'
+
+driver.get(baseDiningURL)
+
+# Get list of dining court URLs #
+# CURRENTLY: Scrapes for current data, need to do breakfast, lunch, dinner #
+# NEXT: Scrape for whole week #
+
+# Get dining court urls
+driverCourtURLs = driver.find_elements(By.CLASS_NAME, "menus__home-content--link")
+for driverCourtURL in driverCourtURLs:
+    courtURLs.append(driverCourtURL.get_attribute('href')) 
+
+#  DINING LIST =[[0-Date, 1-Meal type, 2-Meal Time, 3-Court, 4-Station, 5-Food], [...]]
+
+# Get menu
+for courtURL in courtURLs: # Iterate through dining courts
+    driver.get(courtURL)
+    courtName = driver.find_element(By.XPATH, '//*[@id="app"]/div/header/div/div[1]/a/h1').text
+    court = {}
+    
+    # TODO HERE iterate through meal times
+    for station in driver.find_elements(By.CLASS_NAME, "station"): # Iterate through stations in dining court
+        stationName = station.find_element(By.CLASS_NAME, "station-name").text
+        dates = station.find_elements(By.XPATH, '//*[@id="main"]/div[1]/div[1]/div[2]/div[3]') # Gets date menu webdriver items
+        for date in dates: # TODO HERE iterate through dates
+            print(date.get_attribute('href'))
+            for foodItem in station.find_elements(By.CLASS_NAME, "station-item-text"): # Iterate through food items in station
+                if stationName in court: # just fill out DINING LIST at core, use current for loop var names
+                    court[stationName].append(foodItem.text)
+                else:
+                    court[stationName] = [foodItem.text]
+    courts[courtName] = court
+
+# HOURS #
+
+# Close Webdriver #
+driver.close()
+
+##
+# SQL
+##
+
+#Format data to insert into MySQL tables
+calendarData = [[x] + [y] + [z] + [w] for x,y,z,w in zip(*[iter(dates)], *[iter(weekdays)], *[iter(descriptions)], *[iter(times)])]
+
+try:
+ 
+    #Connect to database with our login
+    mydb = mysql.connector.connect(host="boilermakerbuddydb.c8jck5ubwnj5.us-east-1.rds.amazonaws.com",\
     user="admin", password="ECE49595!", database="boilermakerbuddydb")
 
-# print(mydb)
+    mycursor = mydb.cursor()
 
-mycursor = mydb.cursor()
+    #Clear tables
+    clearDatesSQL = "TRUNCATE TABLE student_calendar"
+    mycursor.execute(clearDatesSQL)
+    print("All records cleared in student_calendar table")
 
-# sql = "INSERT INTO student_calendar (event_date) VALUES %r" %tuple(dates)
+    #Insert data into tables
+    studentCalendarSQL = "INSERT INTO student_calendar (event_date, event_dow, event_description, event_time) VALUES (%s, %s, %s, %s)"
 
-datesql = "INSERT INTO student_calendar (event_date) VALUES (%s)"
-dowsql = "INSERT INTO student_calendar (event_dow) VALUES ('mon')"
-# mycursor.executemany(datesql, data)
+    mycursor.executemany(studentCalendarSQL, calendarData)
 
-mydb.commit()
+    mydb.commit()
 
-print(mycursor.rowcount, "record inserted.")
+    print(mycursor.rowcount, "records inserted in student_calendar table.")
+
+except mysql.connector.Error as error:
+    print("Failed to insert into MySQL table {}".format(error))
+
+finally:
+    #Terminate connection upon completion
+    if mydb.is_connected():
+        mycursor.close()
+        mydb.close()
+        print("MySQL connection is closed")
