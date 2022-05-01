ba4403d2b2c4cf35123f364f5ee591fe3d91494d
stadler <stadler@purdue.edu>
2022-05-01 16:59:14 -0400

    implemented date query handling for date ranges

--- query_database.py
+++ query_database.py
@@ -63,17 +63,52 @@ def selectDateGivenDescription(mycursor, event_description):
         mycursor.execute(studentCalendarSQL)
 
         studentCalendarData = mycursor.fetchall()
+        if(studentCalendarData == []):
+            return "Date not found"
         if(DEBUG > 0):
             print(studentCalendarData)
             print(studentCalendarData[0][0])
             print(len(studentCalendarData))
         dateString = ""
         for a in range(len(studentCalendarData)): #if an event has multiple dates
-            if(a > 0):
+            if (a > 0 ) and (str(studentCalendarData[0][0]) == "2021-11-24" or str(studentCalendarData[0][0]) == "2021-10-11" or str(studentCalendarData[0][0]) == "2021-12-13" or str(studentCalendarData[0][0]) == "2022-03-14" or str(studentCalendarData[0][0]) == "2022-05-02"):
+                dateString = dateString + " to "
+            elif(a > 0) and not(" to " in dateString):
                 dateString = dateString + " and "
+            elif(a > 0) and "to" in dateString:
+                if a % 2 == 0:
+                    dateString = dateString + " and "
             dateS = dateToString(str(studentCalendarData[a][0]))
             dateString = dateString + dateS
         return dateString
+
+    except mysql.connector.Error as error:
+        print("Failed to insert into MySQL table {}".format(error))
+
+##
+# Select date given description
+##
+def selectFoodGivenCourt(mycursor, diningCourt, mealtime):
+
+    try:
+        # Select data from tables
+        #TODO edit this
+        menuSQL = "SELECT event_date FROM student_calendar WHERE event_description = ;"
+
+        mycursor.execute(menuSQL)
+
+        menuData = mycursor.fetchall() # returns a list of foods
+        if(DEBUG > 0):
+            print(menuData)
+            #print(studentCalendarData[0][0])
+            print(len(menuData))
+        foodString = ""
+        for a in range(len(menuData)): #for multiple foods
+            if(a > 0): #add comma but not before 1st item
+                foodString = foodString + ", "
+            food = "apples" #TODO get food from menuData list
+            foodString = foodString + food
+        return foodString
 
     except mysql.connector.Error as error:
         print("Failed to insert into MySQL table {}".format(error))
@@ -89,14 +124,19 @@ def disconnect(mydb, mycursor):
         print("MySQL connection is closed")
 
 def queryDate(event):
-
     mydb, mycursor = connect()
     date = selectDateGivenDescription(mycursor, str(event))
     disconnect(mydb, mycursor)
     return date
 
+def queryMenu(diningcourt, mealtime):
+    mydb, mycursor = connect()
+    foodList = selectFoodGivenCourt(mycursor, str(diningcourt), str(mealtime))
+    disconnect(mydb, mycursor)
+    return foodList
+
 if __name__ == "__main__":
     mydb, mycursor = connect()
-    date = selectDateGivenDescription(mycursor, "CLASSES END")
+    date = selectDateGivenDescription(mycursor, "SPRING VACATION")
     print(date)
     disconnect(mydb, mycursor)
